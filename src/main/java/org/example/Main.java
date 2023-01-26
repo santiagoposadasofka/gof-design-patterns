f28bdda4c32637cd49080b08651f5ff4f09d3b33
Gretty Mosquera <gretymosquera@gmail.com>
2023-01-26 17:10:07 -0500

    Se agregan las instancias en las clases del patrón de creación

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import org.example.creacion.Builder;
+import org.example.creacion.Factory;
+import org.example.creacion.Singleton;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!");
+        System.out.println("Patrones de diseño - GOF");
+
+        System.out.println("Patrones de creación");
+        //System.out.println("Factory");
+        Factory factory = new Factory();
+        //factory.ejecutrarFactory();
+
+        //System.out.println("Builder");
+        Builder builder = new Builder();
+        //builder.ejecutarBuilder();
+
+        System.out.println("Singleton");
+        Singleton singleton = new Singleton();
+        singleton.ejecutarSingleton();
+        /**
+        System.out.println("Patrones de estructurales");
+
+        System.out.println("Patrones de comportamiento");
+         */
     }
 }
--- src/main/java/org/example/creacion/Factory.java
+++ src/main/java/org/example/creacion/Factory.java
@@ -1,19 +1,42 @@
 package org.example.creacion;
 
 public class Factory {
-
     /**
      * Simplifica el proceso de construccion de objetos,
      * en este caso por medio de un parametro "tipo" construimos el objeto esperado.
-     * */
+     */
+
+    /**
+     * Se instancia (crea objeto) de la clase FiguraGeometricaFactory
+     */
+    FiguraGeometricaFactory figuraGeometricaFactory = new FiguraGeometricaFactory();
+
+    /**
+     * Método que permite ejecutar el ejercicio de la clase FiguraGeometricaFactory
+     */
+    public void ejecutrarFactory() {
+        FiguraGeometrica cuadrado = figuraGeometricaFactory.crearFiguraGeometrica("cuadrado");
+        cuadrado.dibujar();
+
+        FiguraGeometrica triangulo = figuraGeometricaFactory.crearFiguraGeometrica("triangulo");
+        triangulo.dibujar();
+
+        FiguraGeometrica circulo = figuraGeometricaFactory.crearFiguraGeometrica("circulo");
+        circulo.dibujar();
+    }
 }
 
-
+/**
+ * Interface de figura geométrica
+ */
 
 interface FiguraGeometrica {
     void dibujar();
 }
 
+/**
+ * Clase de la figura cuadrado que implementa los métodos de figura geométrica
+ */
 class Cuadrado implements FiguraGeometrica {
     @Override
     public void dibujar() {
@@ -21,6 +44,9 @@ class Cuadrado implements FiguraGeometrica {
     }
 }
 
+/**
+ * Clase de la figura triangulo que implementa los métodos de figura geométrica
+ */
 class Triangulo implements FiguraGeometrica {
     @Override
     public void dibujar() {
@@ -28,12 +54,19 @@ class Triangulo implements FiguraGeometrica {
     }
 }
 
+/**
+ * Clase de la figura círculo que implementa los métodos de figura geométrica
+ */
 class Circulo implements FiguraGeometrica {
     @Override
     public void dibujar() {
         System.out.println("Dibujando un círculo");
     }
 }
+
+/**
+ * Clase FiguraGeometricaFactory donde se definen las diferentes figuras geométricas y se crean sus respectivos objetos
+ */
 
 class FiguraGeometricaFactory {
     public FiguraGeometrica crearFiguraGeometrica(String tipo) {
@@ -46,7 +79,7 @@ class FiguraGeometricaFactory {
             return new Triangulo();
         } else if (tipo.equalsIgnoreCase("CIRCULO")) {
             return new Circulo();
-        }else{
+        } else {
             return null;
         }
     }
