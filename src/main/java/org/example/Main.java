73f14d6b36296c1e12da8dabecbb68029f0bde7a
nunezjose770 <nunezjose770@gmail.com>
2023-01-26 18:26:42 -0500

    Implement factory patron

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,17 @@
 package org.example;
 
 import org.example.creacion.Builder;
+import org.example.creacion.Factory;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("-----Pastron builder---------");
+        System.out.println("-----Patron builder---------");
         Builder builder = new Builder();
         builder.ejecutar();
-        System.out.println("----------------------------");
+        System.out.println("------Patron factory---------");
+        Factory factory = new Factory();
+        factory.ejecutar();
+        System.out.println("-----------------------------");
+
     }
 }
