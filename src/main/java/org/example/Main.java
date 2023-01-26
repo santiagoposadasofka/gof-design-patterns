6bdcdeddb34bd35a207a84f069e8a6c4882327d4
SergioBalca <SergioBalca@github.com>
2023-01-26 16:13:05 -0500

    Crear método preparSanduche para ejecutar patrón Build desde método main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.creacion.Builder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Builder builder = new Builder();
+        builder.prepararSanduche();
+
     }
 }
