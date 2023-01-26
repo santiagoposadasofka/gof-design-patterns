579454d4b123e21ec51303f9ab5bd62657958abd
JessiLopezObando <jessicaandrealopezobando@gmail.com>
2023-01-26 17:28:29 -0500

    Interactuando con Builder

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.example.creacion.Builder;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("-----------Builder----------------");
+        Builder builder = new Builder();
+        builder.ejecutar();
     }
 }
