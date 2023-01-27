d1042d558b6e762a64a7d5820a3c59a7e761c4be
Francy Ramirez <franrar95@gmail.com>
2023-01-27 01:51:23 -0500

    Estructura completo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,9 @@ import org.example.creacion.Factory;
 
 import org.example.creacion.Builder;
 import org.example.creacion.Singleton;
+import org.example.estructura.Adapter;
+import org.example.estructura.Decorator;
+import org.example.estructura.Proxy;
 
 
 public class Main {
@@ -19,6 +22,15 @@ public class Main {
 
         Singleton singleton = new Singleton();
         singleton.ejeccut();
+
+        Adapter adapter = new Adapter();
+        adapter.ejecu();
+
+        Decorator decorator = new Decorator();
+        decorator.ejecute();
+
+        Proxy proxy = new Proxy();
+        proxy.ejecut();
 
 
 
