5d59bdbef7ca0e62a4ceccdc6042ecc484a9ff7b
CristhianMorenoG <morenocristhian@outlook.com>
2023-01-27 09:55:31 -0500

    Uso de patrones Builder - Decorator - Mediator - Proxy - Startegy

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import org.example.comprtamiento.Mediator;
+import org.example.comprtamiento.Strategy;
+import org.example.creacion.Builder;
 import org.example.creacion.Factory;
 import org.example.creacion.Singleton;
 import org.example.estructura.Adapter;
+import org.example.estructura.Decorator;
+import org.example.estructura.Proxy;
 
 
 public class Main {
@@ -23,6 +28,20 @@ public class Main {
         adapter.archivoSonido();
 
 
+        Builder builder = new Builder();
+        builder.crearSandwich();
+
+        Decorator decorator = new Decorator();
+        decorator.crearBebida();
+
+        Proxy proxy = new Proxy();
+        proxy.mostrarImagen();
+
+        Mediator mediator = new Mediator();
+        mediator.inciarChat();
+
+        Strategy strategy = new Strategy();
+        strategy.comprimirArchivo();
 
     }
 }
