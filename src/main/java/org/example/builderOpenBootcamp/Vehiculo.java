e3e16ffb78183c1b97a71040c1afb5c2e498919f
SantyOkami23 <santiago.ramirez5@utp.edu.co>
2023-01-30 20:12:05 -0600

    Patron Builder ejemplo Open Bootcamp

---
+++ src/main/java/org/example/builderOpenBootcamp/Vehiculo.java
@@ -0,0 +1,33 @@
+package org.example.builderOpenBootcamp;
+
+
+
+public class Vehiculo {
+    public String marca;
+    public String tipo;
+    public String motor;
+    public int puertas;
+
+    private Vehiculo() {}
+
+    public Vehiculo(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public int getPuertas() {
+        return puertas;
+    }
+}
+
